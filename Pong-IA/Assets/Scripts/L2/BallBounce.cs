cfabf80b7e539561cb3e045af60ba0961d6c2026
ALI Aamir <ibby.star.111@gmail.com>
2017-11-28 12:34:08 +0700

    Velocity change bug-fixing in progress

--- Pong-IA/Assets/Scripts/L2/BallBounce.cs
+++ Pong-IA/Assets/Scripts/L2/BallBounce.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class BallBounce : L2SuperClass {
     public GameObject A, B, C; //3 vectors being used for calculating the cross-product
     Vector3 normal;
-    bool bounced = false;
 
     // Update is called once per frame
     void Update ()
@@ -13,21 +12,32 @@ public class BallBounce : L2SuperClass {
         //performing the cross-product to find the normal of the plane of the paddle.
         Vector3 ba = A.transform.position - B.transform.position;
         Vector3 bc = C.transform.position - B.transform.position;
-        normal = Vector3.Cross(ba, bc);
+		normal = Vector3.Cross (ba, bc);
 	}
 
-    private void OnTriggerEnter(Collider other)
+	private void OnTriggerEnter (Collider other) //Performs the collisions using the cross-product calculated above.
     {
-        if (other.tag.Equals("Ball") && !bounced)
+		if (other.tag.Equals ("Ball"))
         {
-            bounced = true;
-            Debug.Log("Entering\n"+normal+"\n"+other.GetComponent<Rigidbody>().velocity);
-            other.GetComponent<Rigidbody>().velocity = Vector3.Reflect(other.GetComponent<Rigidbody>().velocity, normal);
+			Vector3 uVel = other.GetComponent<Rigidbody> ().velocity;
+			Debug.Log ("Initial vel: " + other.GetComponent<Rigidbody> ().velocity);
+
+			other.GetComponent<Rigidbody> ().velocity = Vector3.Reflect (other.GetComponent<Rigidbody> ().velocity, normal);
+			Vector3 vVel = other.GetComponent<Rigidbody> ().velocity;
+			other.GetComponent<Rigidbody> ().velocity = UnitVector (other.GetComponent<Rigidbody> ().velocity);
+
+			Debug.Log ("End vel: " + other.GetComponent<Rigidbody> ().velocity);
+			Debug.Log ("v Difference: " + (vVel - uVel));
+			Debug.Log ("V Ratio: " + (vVel.magnitude / uVel.magnitude));
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        bounced = false;
-    }
+	private Vector3 UnitVector (Vector3 endVel)
+	{
+		float endVelUnit = endVel.magnitude / uMag;
+		Vector3 ret = endVel / endVelUnit;
+		Debug.Log (uMag);
+		Debug.Log (ret.magnitude);
+		return ret;
+	}
 }
--- Pong-IA/Assets/Scripts/L2/L2SuperClass.cs
+++ Pong-IA/Assets/Scripts/L2/L2SuperClass.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class L2SuperClass : MonoBehaviour {
 
+	//In case I need the rotation of the paddle for any calculations
 	public static Vector3 PaddleRotation = Vector3.zero;
-	public static float xVel = 2, yVel = 2;
+
+	//Preset velocity for the ball allows for hardcoded computations.
+	public static float xVel = 2f, yVel = 2f;
+
+	//Unit vector for the ball's velocity at the start.
+	private static Vector3 unit = new Vector3 (xVel, yVel, Vector3.zero);
+
+	//Magnitude of the unit vector.
+	public static float uMag = Vector3.Magnitude (unit);
 
 	void Start(){
 		QualitySettings.antiAliasing = 1;
